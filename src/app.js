5201e6979c2e162e0bd8c32e004530c9b092f686
Nishat Hoque <hoquen0397@gmail.com>
2021-04-02 16:23:48 -0400

    thoughts service

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ThoughtsService = require('./thoughts-service')
 
 const app = express()
 
@@ -14,6 +15,15 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
+
+app.get('/thoughts', (req,res,next) => {
+    knexInstance = req.app.get('db')
+    ThoughtsService.getAllThoughts(knexInstance)
+        .then(thoughts => {
+            res.json(thoughts)
+        })
+        .catch(next)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
---
+++ src/thoughts-service.js
@@ -0,0 +1,37 @@
+const ThoughtsService = {
+    getAllThoughts(knex) {
+        return knex.select('*').from('thoughtful_thoughts');
+    },
+
+    insertThought(knex, newThought){
+        return knex 
+            .insert(newThought)
+            .into('thoughtful_thoughts')
+            .returning('*')
+            .then(rows => {
+                return rows[0];
+            });
+    },
+
+    getById(knex, id){
+        return knex 
+            .from('thoughtful_thoughts')
+            .select('*')
+            .where('id', id)
+            .first();
+    },
+
+    deleteNote(knex, id){
+        return knex('thoughtful_thoughts')
+            .where('id', id)
+            .delete();
+    },
+
+    updateNote(knex, id, newThoughtField){
+        return knex('thoughtful_thoughts')
+            .where('id', id)
+            .update(newThoughtField);
+    }
+}
+
+module.exports = ThoughtsService
